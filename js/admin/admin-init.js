a82caf2b74da1b57da04947eeead46132b7de359
SyndoriaMiku <61247021+SyndoriaMiku@users.noreply.github.com>
2025-10-01 17:26:43 +0700

    `wip` fixing path

--- js/admin/admin-init.js
+++ js/admin/admin-init.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
-    // Get base path for GitHub Pages compatibility
-    const basePath = window.location.pathname.includes('syndoriamiku.github.io') ? '/syndoriamiku.github.io' : '';
-    
     // Header load
-    $("#header-placeholder").load(basePath + "/static/header.html");
+    $("#header-placeholder").load("static/header.html");
     // Admin nav load
-    $("#nav-admin-placeholder").load(basePath + "/static/nav_admin.html", function() {
+    $("#nav-admin-placeholder").load("static/nav_admin.html", function() {
         // Trigger an event when nav is loaded
         $(document).trigger('adminNavLoaded');
     });
--- js/init.js
+++ js/init.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    // Use relative paths from the current page
-    const basePath = window.location.pathname.includes('syndoriamiku.github.io') ? '/syndoriamiku.github.io' : '';
-    $("#dialog-placeholder").load(basePath + "/static/dialog_notice.html");
-    $("#spinner-placeholder").load(basePath + "/static/spinner.html");
-    $("#header-placeholder").load(basePath + "/static/header.html");
+    $("#dialog-placeholder").load("static/dialog_notice.html");
+    $("#spinner-placeholder").load("static/spinner.html");
+    $("#header-placeholder").load("static/header.html");
 });
